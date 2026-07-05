e25e27ed4585df288d6ce390e4aad62e646da5ed
l7Saint <santiestudio765@gmail.com>
2026-07-05 20:21:10 -0300

    Arreglado bugs de signin

--- sql/init.sql
+++ sql/init.sql
@@ -17,11 +17,31 @@ CREATE TABLE IF NOT EXISTS Vehiculo (
 	tamano VARCHAR(15) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS J_Usuario_Vehiculo (
+	FK_id_Usuario INT,
+	FK_id_Vehiculo INT,
+	FOREIGN KEY (FK_id_Usuario) REFERENCES Usuario(id),
+	FOREIGN KEY (FK_id_Vehiculo) REFERENCES Vehiculo(id),
+	PRIMARY KEY(FK_id_Usuario, FK_id_Vehiculo),
+);
+
+//por ahora solo necesitamos estas 3 tablas para el login
+/*
 CREATE TABLE IF NOT EXISTS Ubicacion (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	numero_puerta INT NOT NULL,
 	//esquina capaz que no, depende, y tambien depende como se tendria que guardar.
 	calle VARCHAR(40) NOT NULL,
-	latitud DOUBLE NOT NULL,
-	longitud DOUBLE NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS ViaPublica (
+	latitud DOUBLE NOT NULL,
+	longitud DOUBLE NOT NULL,
+	PRIMARY KEY(latitud, longitud)
+);
+
+CREATE TABLE IF NOT EXISTS EstacionamientoPrivado (
+	horarios 
+	contacto VARCHAR(50)
+);
+*/
