1f58bee248c36bf7ab6683eb6c0aab8e9d698653
l7Saint <santiestudio765@gmail.com>
2026-07-15 10:53:44 -0300

    empezado la conexion con la base de datos

--- src/public/signin/signin.php
+++ src/public/signin/signin.php
@@ -6,7 +6,6 @@ $DIAS = array(
 	"Jueves",
 	"Viernes"
 );
-
 $HORAS = array(
 	"8:00 - 8:40",  // 0
 	"8:40 - 9:20",  // 1
@@ -26,7 +25,7 @@ $HORAS = array(
 $nombrePersona = $_POST['nombre'];
 $apellidoPersona = $_POST['apellido'];
 $email = $_POST['email'];
-$contraseñaUsuario = $_POST['contraseña'];
+$contrasenaUsuario = $_POST['contraseña'];
 $marca_modeloAuto = $_POST['marca_modelo'];
 
 $horarios = [];
@@ -36,95 +35,14 @@ for($ndia = 0; $ndia < count($DIAS); $ndia++){
 	$horariosDiaPOST = "horarios" . $DIAS[$ndia];
 	array_push($horarios, $_POST[$horariosDiaPOST] ?? []); /*Los signos de preguntan funcionan como un if, si el usuario puso una opción recibe el dato, si lo dejó vacio, para que no de un error, lo deja vacío.*/
 }
+
+require_once("/config/conexion.php");
+$sql = "INSERT INTO Usuario(nombre, apellido, email, contrasena)
+	VALUES (?, ?, ?)"
+
+$stmt = conexion->prepare($sql);
+$stmt->execute([$nombrePersona, $apellidoPersona, $email, $contrasenaUsuario]};
+$header("Location: /public/login/index.html");
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Datos</title>
-	<link rel="stylesheet" href="../../assets/css/main_style.css"/>
-	<style>
-#cajaDatos{
-  background-color: whitesmoke;
-      border-radius: 15px;
-      padding-top: 1rem;
-      padding-bottom: 1rem;
-      width: 30rem;
-}
-
-.etiqueta{
-       color: #67878a;
-      font-family: "Open Sans", sans-serif;
-}
-
-.btn{
-      color: #285357;
-      font-weight: bolder;
-      cursor: pointer;
-      background-color: #67878a;
-      border-radius: 10px;
-      border: 2px 2px solid;
-      border-color: #67878a;
-      padding: 0.5rem;
-}
-
-.btn:hover{
-    background-color: #455c5e;
-      color: whitesmoke;
-}
-
-.textoPregunta{
-    color:black;
-    font-family: "Open Sans", sans-serif;
-    font-weight: bold;
-    text-decoration: underline;
-
-}
-</style>
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-	<script src="../../assets/js/cargarNavbarLogin.js"></script>
-</head>
-<body>
-    <center>
-    <div id="cajaDatos">
-
-    <h1 class = "titulos">Datos de registro:</h1>
-    <p class="textoPregunta">¿Estos datos son correctos?</p>
-
-<?php
-echo "<p class='dato'><b class='etiqueta'>Nombre:</b> $nombrePersona</p>";
-echo "<p class='dato'><b class='etiqueta'>Apellido:</b> $apellidoPersona</p>";
-echo "<p class='dato'><b class='etiqueta'>Correo electrónico:</b> $email</p>";
-echo "<p class='dato'><b class='etiqueta'>Contraseña (prueba):</b> $contraseñaUsuario</p>";
-echo "<p class='dato'><b class='etiqueta'>Marca y modelo del auto:</b> $marca_modeloAuto</p>";
-echo "<p class='dato'><b class='etiqueta'>Horarios:</b></p>";
-for($ndia = 0; $ndia < count($horarios); $ndia++){
-	$horariosDia = $horarios[$ndia];
-	if(count($horariosDia) != 0){
-		echo $DIAS[$ndia] . "</br>";
-		for($nhora = 0; $nhora < count($horariosDia); $nhora++){
-			echo $HORAS[$nhora] . "</br>";
-		}
-	}
-	echo "</br>";
-}
-?>
-
-<a href="index.html" class="btn">Volver</a>
-<a href="../inicio/index.html" class="btn">Completar registro</a>
-
-
-</div>
-</center>
-
-<?php 
-#var_dump($horarios); 
-#var_dump($_POST["horariosLunes"]); 
-?>
-</body>
-</html>
 
