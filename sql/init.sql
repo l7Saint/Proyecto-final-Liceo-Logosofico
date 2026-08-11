fcd9b5c253178eee58aeb92482194c28d5715b5e
l7Saint <santiestudio765@gmail.com>
2026-08-11 08:47:36 -0300

    Arreglado el script de creacion de base de datos

--- sql/init.sql
+++ sql/init.sql
@@ -7,41 +7,45 @@ CREATE TABLE IF NOT EXISTS Usuario (
 	apellido VARCHAR(40) NOT NULL,
 	email VARCHAR(150) NOT NULL,
 	contrasena VARCHAR(255) NOT NULL,
-	fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	fechaRegistro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS Vehiculo (
+CREATE TABLE IF NOT EXISTS Auto (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	matricula VARCHAR(8) NOT NULL,
 	color VARCHAR(15) NOT NULL,
 	tamano VARCHAR(15) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS J_Usuario_Vehiculo (
+CREATE TABLE IF NOT EXISTS J_Usuario_Auto (
 	FK_id_Usuario INT,
-	FK_id_Vehiculo INT,
+	FK_id_Auto INT,
+
 	FOREIGN KEY (FK_id_Usuario) REFERENCES Usuario(id),
-	FOREIGN KEY (FK_id_Vehiculo) REFERENCES Vehiculo(id),
-	PRIMARY KEY(FK_id_Usuario, FK_id_Vehiculo),
+	FOREIGN KEY (FK_id_Auto) REFERENCES Auto(id),
+
+	PRIMARY KEY(FK_id_Usuario, FK_id_Auto)
 );
 
-//por ahora solo necesitamos estas 3 tablas para el login
-/*
 CREATE TABLE IF NOT EXISTS Ubicacion (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	numero_puerta INT NOT NULL,
-	//esquina capaz que no, depende, y tambien depende como se tendria que guardar.
-	calle VARCHAR(40) NOT NULL,
+	numeroPuerta INT,
+	calle VARCHAR(100) NOT NULL,
+	esquina VARCHAR(100),
+	latitud REAL,
+	longitud REAL
 );
 
-CREATE TABLE IF NOT EXISTS ViaPublica (
-	latitud DOUBLE NOT NULL,
-	longitud DOUBLE NOT NULL,
-	PRIMARY KEY(latitud, longitud)
-);
+CREATE TABLE IF NOT EXISTS Estaciona (
+	FK_id_Usuario INT,
+	FK_id_Vehiculo INT,
+	FK_id_Ubicacion INT,
+	horaInicio TIMESTAMP DEFUALT CURRENT_TIMESTAMP,
+	horaFin TIMESTAMP,
 
-CREATE TABLE IF NOT EXISTS EstacionamientoPrivado (
-	horarios 
-	contacto VARCHAR(50)
+	FOREIGN KEY (FK_id_Usuario) REFERENCES J_Usuario_Auto(FK_id_Usuario),
+	FOREIGN KEY (FK_id_Ubicacion) REFERENCES J_Usuario_Auto(id),
+	FOREIGN KEY (FK_id_Auto) REFERENCES J_Usuario_Auto(FK_id_Auto),
+
+	PRIMARY KEY(FK_id_Usuario, FK_id_Auto, horaInicio, horaFin)
 );
-*/
