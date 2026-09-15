58a9fac92a5e40cd762fd16439e0755218932c2f
l7Saint <santiestudio765@gmail.com>
2026-09-15 17:44:37 -0300

    Fix: Clave primaria de Horarios

--- sql/init.sql
+++ sql/init.sql
@@ -21,9 +21,10 @@ CREATE TABLE IF NOT EXISTS Vehiculo (
 );
 
 CREATE TABLE IF NOT EXISTS Horario (
-	numero_hora INT PRIMARY KEY,
+	numero_hora INT,
 	dia_semana ENUM('lunes','martes','miercoles','jueves','viernes','sabado') NOT NULL,
-	horario VARCHAR(15) NOT NULL
+	horario VARCHAR(15) NOT NULL,
+	PRIMARY KEY(numero_hora, dia_semana)
 );
 
 CREATE TABLE IF NOT EXISTS Ubicacion (
