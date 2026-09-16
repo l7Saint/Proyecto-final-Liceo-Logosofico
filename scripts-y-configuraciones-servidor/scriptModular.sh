491694c69edb9bb224c653daaede6db4c0015c25
jjulietaa <juliymanchi28@gmail.com>
2026-09-16 20:55:13 -0300

    Creación de script modular para el servidor

---
+++ scripts-y-configuraciones-servidor/scriptModular.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+menu(){
+echo -e "Bienvenid@ al script modular del servidor\nOpciones:"
+echo -e "1)Gestión de usuarios y grupos\n2)Configuración de firewall\n3)Respaldo\n4)Logs\n5)Salir"
+read -p "Escriba un número para acceder al script:" opcionScript
+}
+
+while [[ $opcionUser != 5 ]];do
+menuOpciones
+case $opcionScript in
+1)gestion_usuarios.sh;;
+2)firewall.sh;;
+3)scriptRespaldoCS.sh;;
+4)logsFedora.sh;;
+5)
+esac
+done
+
